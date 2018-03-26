130a52f8ff18c66765068630bf5f69592247a5c9
ryan <ryanjchesnut@gmail.com>
2018-03-25 23:15:50 -0400

    Added palindrome function

--- algorithms.js
+++ algorithms.js
@@ -14,4 +14,18 @@ function reverseString(str) {
     return str;
 }
 
+function palindrome(str) {
+    str = str.toLowerCase();
+    str = str.replace(/\s|_|\.|,|-|\(|\)|\\|\/|\||:|/g, '');
+    strArray = str.split("");
+    halfBoundry = strArray.length / 2;
+    var j = strArray.length - 1;
+    for (var i = 0; i < halfBoundry; i++) {
+        if (strArray[i] !== strArray[j]) {
+            return false;
+        }
+        j--;
+    }
+    return true;
+}
 
