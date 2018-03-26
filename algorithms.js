9b4f82cb52d5a560b99ca3e43530042892cd9094
ryanchesnut <37148876+ryanchesnut@users.noreply.github.com>
2018-03-26 10:48:06 -0400

    Update algorithms.js
    added a "find largest number in array" function.

--- algorithms.js
+++ algorithms.js
@@ -52,3 +52,17 @@ function titleCase(str) {
     return str;
 }
 
+function largestOfFour(arr) {
+  var maxArray = []; 
+  for(var i = 0; i < arr.length; i++){
+    var maxNumber = 0;
+    for(var j = 0; j < arr[i].length; j++){
+      if ( arr[i][j] > maxNumber){
+        maxNumber =  arr[i][j];
+        maxArray[i] = maxNumber; 
+      }
+    }
+  }
+  return maxArray;
+}
+
