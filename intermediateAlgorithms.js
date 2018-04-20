95f30378ddce28c4407986af16be5084ad4b8606
ryan <ryanjchesnut@gmail.com>
2018-04-20 00:25:21 -0400

    Diff Two Arrays
    Added Diff Two Arrays Algorithm

--- intermediateAlgorithms.js
+++ intermediateAlgorithms.js
@@ -1,4 +1,4 @@
-
+// Sum All Numbers in a Range Complete  
 function sumAll(arr) {
     var sum = 0;
     arr.reduce(function red(a, b) {
@@ -9,6 +9,25 @@ function sumAll(arr) {
         }
     });
     return sum;
+}
+
+// Diff Two Arrays
+function diffArray(arr1, arr2) {
+    var newArr = [];
+    var tempArr1 = [];
+    var tempArr2 = [];
+
+    tempArr1 = arr1.filter(function (x) {
+        return (arr2.indexOf(x) < 0);
+    });
+
+    tempArr2 = arr2.filter(function (y) {
+        return (arr1.indexOf(y) < 0);
+    });
+
+    newArr = tempArr1.concat(tempArr2);
+
+    return newArr;
 }
 
 
